cd44022ecc6f0f8807cb3792f9f9d709f4a8367d
cheinisch <IbhesPfG!01>
2017-05-31 12:50:31 +0200

    Datenupdate funktioniert endlich

--- app/src/main/java/de/christian_heinisch/petcheck/OverviewFragment.java
+++ app/src/main/java/de/christian_heinisch/petcheck/OverviewFragment.java
@@ -16,6 +16,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -109,9 +110,19 @@ public class OverviewFragment extends Fragment {
         String jsonname = ((MainActivity)getActivity()).getpetdata();
 
         String json_file = jsonname + "-data.json";
+        String FILE_NAME = jsonname + ".json";
         try {
 
-            InputStream is = getContext().getAssets().open(json_file);
+            InputStream is;
+
+            //File file = new File("rabbit.json");
+            //if(file.exists()){
+                //System.out.println("file is already there");
+                is = getActivity().openFileInput(FILE_NAME);
+            //}else{
+                System.out.println("Not find file ");
+              //  is = getContext().getAssets().open(json_file);
+            //}
 
             int size = is.available();
             byte[] buffer = new byte[size];
